7bc32d9fdbd72c019af43c1c4abae94f9112a8e9
LuisEnVilla <villak@hotmail.es>
2016-01-11 17:35:24 -0600

    Alert

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 	$('.collapsible').collapsible({
 		accordion: false
 	});
+	$('select').material_select();
 });
+
 //::::::::::::::::::::::::::::::::: Node_Modules
 var net = require('net');
 var moment = require('moment');
@@ -13,7 +15,20 @@ global.datGL200 = {
 	port: '8090',
 	protocolVersion: '02010B',
 	uniqueID: '135790246811220',
-	deviceName: 'LoremGL200'
+	deviceName: 'LoremGL200',
+	batteryPercentage: 80,
+	GPSUTCtime: moment().format('YYYYMMDDHHmmss'),
+	Speed: "127.5",
+	Longitude: "-98.707299",
+	Latitude: "20.102827",
+	Altitude: "00340.5",
+	GPSaccuracy: 30,
+	Azimuth: 150,
+	MCC: "0334",
+	MNC: "0020",
+	LAC: "FFFF",
+	CellID: "1234",
+	OdoMileage: "4294967.5"
 };
 //:::::::::::::::::::::::::::::::::: Conect Socket TCP
 function conectSocket() {
@@ -21,6 +36,7 @@ function conectSocket() {
 	var portSocket = $('#portSocket').val();
 	if (hostSocket != "")
 		global.datGL200.host = hostSocket;
+	//global.datGL200.host = hostSocket || ;
 	if (portSocket != "")
 		global.datGL200.port = portSocket;
 	global.socket = net.connect({
@@ -31,7 +47,7 @@ function conectSocket() {
 		$('#hostSocket').attr("placeholder", global.datGL200.host);
 		$('#portSocket').attr("placeholder", global.datGL200.port);
 	});
-}
+};
 
 //:::::::::::::::::::::::::::::::::::: Send +RESP:GTPNA
 function gtpna() {
@@ -58,4 +74,48 @@ function gtpna() {
 	$('#IMEI').attr("placeholder", global.datGL200.uniqueID);
 	$('#deviceName').attr("placeholder", global.datGL200.deviceName);
 	$('#protocolVersion').attr("placeholder", global.datGL200.protocolVersion);
-}
+};
+
+//:::::::::::::::::::::::::::::::::::::: Get All Input and Generate Trama
+function getAllInput() {
+	global.tramaBaseReport = [];
+	global.tramaBaseReport.push($('#protocolVersion').val() || global.datGL200.protocolVersion);
+	global.tramaBaseReport.push($('#IMEI').val() || global.datGL200.uniqueID);
+	global.tramaBaseReport.push($('#deviceName').val() || global.datGL200.deviceName);
+	global.tramaBaseReport.push("0"); // Report ID/Append mask <=4 0 – FFFF +RESP:GTGEO/+RESP:GTFRI
+	global.tramaBaseReport.push("0"); // Report Type 0 | 1  +RESP:GTGEO/+RESP:GTSPD/+RESP:GTNMR 
+	global.tramaBaseReport.push("1"); // Confidential Number of points in one report message. <=2 0-15 +RESP:GTFRI
+	global.tramaBaseReport.push($('#GPSaccuracy').val() || global.datGL200.GPSaccuracy);
+	global.tramaBaseReport.push($('#Speed').val() || global.datGL200.Speed);
+	global.tramaBaseReport.push($('#Azimuth').val() || global.datGL200.Azimuth);
+	global.tramaBaseReport.push($('#Altitude').val() || global.datGL200.Altitude);
+	global.tramaBaseReport.push($('#Longitude').val() || global.datGL200.Longitude);
+	global.tramaBaseReport.push($('#Latitude').val() || global.datGL200.Latitude);
+	global.tramaBaseReport.push($('#GPSUTCtime').val() || global.datGL200.GPSUTCtime);
+	global.tramaBaseReport.push($('#MCC').val() || global.datGL200.MCC);
+	global.tramaBaseReport.push($('#MNC').val() || global.datGL200.MNC);
+	global.tramaBaseReport.push($('#LAC').val() || global.datGL200.LAC);
+	global.tramaBaseReport.push($('#CellID').val() || global.datGL200.CellID);
+	global.tramaBaseReport.push($('#OdoMileage').val() || global.datGL200.OdoMileage);
+	global.tramaBaseReport.push($('#batterypercentage').val() || global.datGL200.batterypercentage);
+	global.tramaBaseReport.push(''); // I/O status <=4 0-FFFF A hexadecimal value to indicate the I/O status.
+	global.tramaBaseReport.push(moment().format('YYYYMMDDHHmmss')); //Send Time
+	global.tramaBaseReport.push('FFFF$'); //Count Number + Tail
+};
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::: Send Report
+function sendReport() {
+	var inputReport = $('#selectReport').val();
+	getAllInput();
+	global.tramaBaseReport[5] = inputReport[inputReport.length - 1];
+	var tramaSend = "+RESP:" + inputReport.substring(0, inputReport.length - 1) + "," + global.tramaBaseReport
+	global.socket.write('' + tramaSend);
+	$(".consol").append(">>" + tramaSend + '<br>');
+};
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::: Console Input
+function consoleInput() {
+	var tramaInput = $('#tramaInput').val();
+	global.socket.write('' + tramaInput);
+	$(".consol").append(">>" + tramaInput + '<br>');
+};
